83f0998cbd254cf32f3b054638e939a2cffd83af
Odysseas Papadimas <odysseas.patra@gmail.com>
2025-11-30 14:51:12 +0100

    fix: Wrap TanStackDevtools in a conditional to ensure it only renders in development mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -54,18 +54,20 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <TanStackDevtools
-          config={{
-            position: "bottom-right",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            TanStackQueryDevtools,
-          ]}
-        />
+        {import.meta.env.DEV && (
+          <TanStackDevtools
+            config={{
+              position: "bottom-right",
+            }}
+            plugins={[
+              {
+                name: "Tanstack Router",
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+              TanStackQueryDevtools,
+            ]}
+          />
+        )}
         <Toaster richColors position="top-right" />
         <Scripts />
       </body>
